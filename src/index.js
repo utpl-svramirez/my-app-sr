5559e41314bc5cd79c052202a4f13c2b07c2dce5
utpl-svramirez <svramirez@utpl.edu.ec>
2024-06-10 23:28:34 -0500

    actualización index.js con componentes de mapa de capacidades

--- src/index.js
+++ src/index.js
@@ -1,17 +1,46 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client"
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+
+function Soft(){
+    return (<h1>SAPSOFT</h1>)   
+}
+
+function Header1(){
+    return <h1>Módulo de Abastecimiento</h1>
+}
+
+function Line1(){
+    return(<h2>Solicitar stock</h2>)
+}
+
+function Line2(){
+    return (<h2>Receptar Stock</h2>)
+}
+
+function Line3(){
+    return (<h2>Actualizar Stock</h2>)
+}
+
+function Header2(){
+    return <h1>Módulo de ventas</h1>   
+}
+
+function Line4(){
+    return (<h2>Registrar venta</h2>)
+}
+
+function Line5(){
+    return (<h2>Generar órden de pedido</h2>)
+}
+//root.render(<h1>Hola Mundo</h1>);
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+    <>
+    <section><Soft/> </section>
+    <section><Header1/> <Line1/> <Line2/> <Line3/> </section>
+    <section><Header2/><Line4/><Line5/> </section>
+    </>
+    );
